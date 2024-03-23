97a2c2c7c427a70bdb40fde7c60f8684ca2168ec
DivyarajSunva <Divyarajsunva12@gmail.com>
2024-03-23 15:46:31 +0530

    List modal & Color picker

--- App.js
+++ App.js
@@ -1,20 +1,105 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  FlatList,
+  Modal,
+} from "react-native";
+import Colors from "./Colors.js";
+import { AntDesign } from "@expo/vector-icons";
+import tempData from "./tempData.js";
+import TodoList from "./Components/TodoList.js";
+import React from "react";
+import AddListModal from "./Components/AddListModal.js";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    addTodoVisible: false,
+  };
+
+  toggleAddTodoModal() {
+    this.setState({ addTodoVisible: !this.state.addTodoVisible });
+  }
+
+  renderList = (list) => {
+    return <TodoList list={list} />;
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Modal
+          animationType="slide"
+          visible={this.state.addTodoVisible}
+          onRequestClose={() => this.toggleAddTodoModal()}
+        >
+          <AddListModal closeModal={() => this.toggleAddTodoModal()} />
+        </Modal>
+        <View style={{ flexDirection: "row" }}>
+          <View style={styles.divider} />
+          <Text style={styles.title}>
+            Todo
+            <Text style={{ fontWeight: 300, color: Colors.blue }}>Lists</Text>
+          </Text>
+          <View style={styles.divider} />
+        </View>
+
+        <View style={{ marginVertical: 48 }}>
+          <TouchableOpacity
+            style={styles.addList}
+            onPress={() => this.toggleAddTodoModal()}
+          >
+            <AntDesign name="plus" size={16} color={Colors.blue} />
+          </TouchableOpacity>
+
+          <Text style={styles.add}>Add List</Text>
+        </View>
+        <View style={{ height: 275, paddingLeft: 32 }}>
+          <FlatList
+            data={tempData}
+            keyExtractor={(item) => item.name}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            renderItem={({ item }) => this.renderList(item)}
+          />
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  divider: {
+    backgroundColor: Colors.lightblue,
+    height: 1,
+    flex: 1,
+    alignSelf: "center",
+  },
+  title: {
+    fontSize: 38,
+    fontWeight: "800",
+    color: Colors.black,
+    paddingHorizontal: 64,
+  },
+  addList: {
+    borderWidth: 2,
+    borderColor: Colors.lightblue,
+    borderRadius: 5,
+    padding: 16,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  add: {
+    color: Colors.blue,
+    fontSize: 14,
+    fontWeight: "600",
+    marginTop: 8,
   },
 });
---
+++ tempData.js
@@ -0,0 +1,68 @@
+export default tempData = [
+  {
+    name: "Plan a trip",
+    color: "#24A6D9",
+    todos: [
+      {
+        title: "Book Flight",
+        completed: false,
+      },
+      {
+        title: "Passport Check",
+        completed: true,
+      },
+      {
+        title: "Reserve Hotel Room",
+        completed: true,
+      },
+      {
+        title: "Pack Luggage",
+        completed: false,
+      },
+    ],
+  },
+  {
+    name: "GYM",
+    color: "#8022D9",
+    todos: [
+      {
+        title: "Back Day",
+        completed: false,
+      },
+      {
+        title: "Arms Day",
+        completed: true,
+      },
+      {
+        title: "Push Day",
+        completed: true,
+      },
+      {
+        title: "Pull Day",
+        completed: false,
+      },
+    ],
+  },
+  {
+    name: "Birthday Party",
+    color: "#5862E0",
+    todos: [
+      {
+        title: "Balloons",
+        completed: false,
+      },
+      {
+        title: "Cake",
+        completed: true,
+      },
+      {
+        title: "Hats",
+        completed: true,
+      },
+      {
+        title: "Send Invitation",
+        completed: false,
+      },
+    ],
+  },
+];
